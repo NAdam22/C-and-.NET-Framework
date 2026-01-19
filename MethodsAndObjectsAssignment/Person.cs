2200dd41d21228118cafa25f61e2047ae07d7813
Naif A <naifissa22@gmail.com>
2026-01-18 23:56:57 -0400

    Add files via upload

---
+++ MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    // Define a base class called Person
+    public class Person
+    {
+        
+    // Property to store the first name of the person
+    public string FirstName { get; set; }
+
+        // Property to store the last name of the person
+        public string LastName { get; set; }
+
+        // Void method that prints the full name of the person
+        public void SayName()
+        {
+            // Write the full name to the console in the format "Name: [full name]"
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+
+    // Define a class called Employee that inherits from Person
+    public class Employee : Person
+    {
+        // Property to store the employee's ID
+        public int Id { get; set; }
+    }
+}
---
+++ MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    // Main program class
+    class Program
+    {
+        // Entry point of the console application
+        static void Main(string[] args)
+        {
+            // Instantiate a new Employee object and initialize its properties
+            Employee employee = new Employee
+            {
+                FirstName = "Sample", // Set the first name
+                LastName = "Student", // Set the last name
+                Id = 1                // Set the employee ID (optional example)
+            };
+
+            // Call the SayName() method inherited from the Person class
+            employee.SayName();
+
+            // Wait for user input before closing the console
+            Console.ReadLine();
+        }
+    }
+}
