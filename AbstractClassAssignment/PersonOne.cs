f3098f049abdbf9efc6fb53b11bf9e8581791934
Naif A <naifissa22@gmail.com>
2026-01-19 00:04:59 -0400

    Add files via upload

---
+++ AbstractClassAssignment/PersonOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    // The abstract class Person
+    // Abstract classes cannot be instantiated directly
+    abstract class PersonOne
+    {
+        // Property to store the first name
+        public string firstName { get; set; }
+
+        // Property to store the last name
+        public string lastName { get; set; }
+
+        // Abstract method that must be implemented by any class that inherits from Person
+        public abstract void SayName();
+    }
+
+
+    // Employee class inherits from the abstract Person class
+    class Employee : PersonOne
+    {
+        // Implementation of the abstract SayName() method
+        // This method displays the full name of the employee
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ AbstractClassAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    // The main program class
+    class Program
+    {
+        // Entry point of the console application
+        static void Main(string[] args)
+        {
+            // Create an Employee object
+            // Assign values to firstName and lastName
+            Employee employee = new Employee()
+        {
+            firstName = "Sample",
+            lastName = "Student"
+        };
+
+            // Call the SayName() method on the employee object
+            employee.SayName();
+
+            // Keeps the console window open until a key is pressed
+            Console.ReadLine();
+        }
+    }
+}
